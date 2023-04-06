6d521246f16422ef4eb50dde81c4985117531352
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-04-06 10:37:22 +0900

    Create PIR.cpp

---
+++ wPi/PIR.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <wiringPi.h>
+using namespace std;
+
+#define PIN_PIR 5
+
+void sensePIR(){
+    static int count = 0;
+    cout << "Somebody sensed " << count++ << '\n';
+}
+
+int main(int argc, char *argv[]){
+    wiringPiSetup();
+    pinMode(PIN_PIR, INPUT);
+
+    wiringPiISR(PIN_PIR, INT_EDGE_RISING, &sensePIR);
+
+    for(;;) {
+        
+    }
+    return 0;
+}
