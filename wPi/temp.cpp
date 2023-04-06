bf839f7e592643602edd48b1c1452add651654a2
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-04-06 11:23:38 +0900

    Update temp.cpp

--- wPi/temp.cpp
+++ wPi/temp.cpp
@@ -23,4 +23,39 @@ TRYAGAIN:
     do{delayMicroseconds(1);}while(digitalRead(DHT_GPIO) == HIGH);
     do{delayMicroseconds(1);}while(digitalRead(DHT_GPIO) == LOW);
     do{delayMicroseconds(1);}while(digitalRead(DHT_GPIO) == HIGH);
+
+    for(int d=0, d < 5; d++){
+        for(int i=0; i < 8; i++){
+            do{delayMicroseconds(1);}
+            int width = 0;
+            do{
+                width++;
+                delayMicroseconds(1);
+                if(width > 1000) break;
+            } while(digitalRead(DHT_GPIO) == HIGH);
+            data[d] = data[d] | ((width > LH_THRESHOLD) << (7-i));
+        }
+    }
+
+    if(USING_DHT11){
+        humid = data[0]*10;
+        temp = data[2]*10;
+    } else {
+        humid = data[0] << 8 | data[1];
+        temp = data[2] << 8 | data[3];
+    }
+
+    unsigned char chk = 0;
+    for(int i=0; i < 4; i++) {chk += data[i]};
+    if(chk == data[4]){
+        cout << "The checksum is good" << endl;
+        cout << "The temperature is " << (float)temp/10 << "C" << endl;
+        cout << "The humidity is " << (float)humid/10 << "%" << endl;
+
+    }else{
+        cout << "Checksum bad - data error - trying again" << endl;
+        usleep(2000000);
+        goto TRYAGAIN;
+    }
+    return 0;
 }
