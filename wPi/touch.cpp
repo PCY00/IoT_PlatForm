2cb38436f759745a160c55392ab2b41d28fec151
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-04-06 10:52:25 +0900

    Create touch.cpp

---
+++ wPi/touch.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <wiringPi.h>
+using namespace std;
+
+#define PIN_TOUCH 5
+#define DEBOUNCE_TIME 200
+
+void senseTouch(){
+    static unsigned long lastISRTime = 0;
+    static unsigned long count = 0;
+    unsigned long currentISRTime = millis();
+    if(currentISRTime - lastISRTime > DEBOUNCE_TIME){
+        cout << "Touch detected: " << count++ << endl;
+    }
+    lastISRTime = currentISRTime;
+}
+
+int main(int argc, char *argv[]){
+    wiringPiSetup();
+    pinMode(PIN_TOUCH, INPUT);
+
+    wiringPiISR(PIN_TOUCH, INT_EDGE_RISING, &senseTouch);
+
+    for(;;) {
+
+    }
+
+    cout << "Finished" << endl;
+    return 0;
+}
