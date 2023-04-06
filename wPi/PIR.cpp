21c5b6259abbbba9f2c6489348adacdc35979cb0
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-04-06 11:01:19 +0900

    Update PIR.cpp

--- wPi/PIR.cpp
+++ wPi/PIR.cpp
@@ -10,7 +10,7 @@ void sensePIR(){
 }
 
 int main(int argc, char *argv[]){
-    wiringPiSetup();
+    wiringPiSetupGpio();
     pinMode(PIN_PIR, INPUT);
 
     wiringPiISR(PIN_PIR, INT_EDGE_RISING, &sensePIR);
